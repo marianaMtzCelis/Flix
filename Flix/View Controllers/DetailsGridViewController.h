157c2ff6cc61b10c97c10778a75edc70254a4fc9
Mariana Martinez <marianamtzcelis@fb.com>
2020-06-25 18:07:30 -0500

    User can tap a poster in the collection view to see a detail screen of that movie

---
+++ Flix/View Controllers/DetailsGridViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailsGridViewController.h
+//  Flix
+//
+//  Created by Mariana Martinez on 25/06/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsGridViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flix/View Controllers/DetailsGridViewController.m
@@ -0,0 +1,63 @@
+//
+//  DetailsGridViewController.m
+//  Flix
+//
+//  Created by Mariana Martinez on 25/06/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import "DetailsGridViewController.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface DetailsGridViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *backdropView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *synopsysLabel;
+
+
+@end
+
+@implementation DetailsGridViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //Poster
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = self.movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+    [self.posterView setImageWithURL:posterURL];
+    
+    //Backdrop
+    NSString *backdropURLString = self.movie[@"backdrop_path"];
+    NSString *fullBackdropURLString = [baseURLString stringByAppendingString:backdropURLString];
+    NSURL *backdropURL = [NSURL URLWithString:fullBackdropURLString];
+    [self.backdropView setImageWithURL:backdropURL];
+    
+    self.titleLabel.text = self.movie[@"title"];
+    self.synopsysLabel.text = self.movie[@"overview"];
+    self.dateLabel.text = self.movie[@"release_date"];
+    
+    [self.titleLabel sizeToFit];
+    [self.synopsysLabel sizeToFit];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
